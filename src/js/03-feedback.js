a35ffc68253d38463ce5082b2638666dac473e92
Grimmjjou <grimmjjou@gmail.com>
2023-03-14 00:28:32 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -19,7 +19,6 @@ function onSubmitForm(e) {
     e.currentTarget.reset();
     console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
     localStorage.removeItem(STORAGE_KEY);
-    formData = {};
   } else {
     alert('Заповніть порожні поля!');
   }
@@ -41,68 +40,3 @@ function getFormOutput() {
     refs.message.value = savedFormData.message || '';
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import throttle from "lodash.throttle";
-
-// const STORAGE_KEY = 'feedback';
-// const formData ={};
-
-// const refs = {
-//     form: document.querySelector('.js-feedback'),
-//     textarea: document.querySelector('.js-feedback textarea'),
-// };
-
-// refs.form.addEventListener('submit', onFormsubmit);
-// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-
-// refs.form.addEventListener('input', e => {
-//     // console.log(e.target.name);
-//     // console.log(e.target.value);
-
-//     formData[e.target.name] = e.target.value;
-//     localStorage.setItem(FORM_STATE, JSON.stringify(formData));
-//     console.log(formData);
-// });
-
-// populateTextarea();
-
-// function onFormsubmit(evt) {
-//     evt.prevenDefault();
-//     console.log('Отправляем форму');
-//     evt.curentTarget.reset();
-//     localStorage.removeItem(STORAGE_KEY);
-// };
-
-// function onTextareaInput(evt) {
-//     const message = evt.target.value;
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(message));
-// };
-
-// function populateTextarea() {
-//     const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-//     if (savedMessage) {
-//         console.log(savedMessage);
-//         refs.textarea.value = savedMessage;
-//     }
-// };
